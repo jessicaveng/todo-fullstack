193b2bb50e64c66393f6accec9b4928a9d455cfd
Htense <68847766+Htense@users.noreply.github.com>
2020-11-30 15:25:40 +1300

    WE DONE THE CRUD!!!

--- server/db/db.js
+++ server/db/db.js
@@ -4,14 +4,38 @@ const connection = require('./connection')
 
 
 
+//GET CRUD R = Read
 
 function getTodos(db = connection) {
   return db('toDoList').select()
 }
 
+//POST CRUD C = Create
+
+function createTask(newTask, db = connection){
+  return db('toDoList').insert(newTask)
+  // inserting & automatically returning an array of ids, but just the first id for whatever task has been inserted,
+  .then(ids => ids[0])
+}
+
+// PATCH CRUD U = UPDATE
+
+function updateTask(id, updatedTask, db = connection ){
+
+  return db('toDoList').update(updatedTask).where('id', id)
+  //update task on the id that we select 
+}
 
 
+function deleteTask(id, db = connection){
+  return db('toDoList').delete().where('id', id)
+}
+
+// CRUD D = DELETE
 
 module.exports = {
   getTodos,
+  createTask,
+  updateTask,
+  deleteTask,
 }
--- server/routes/toDoRoutes.js
+++ server/routes/toDoRoutes.js
@@ -1,26 +1,72 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const {getTodos} = require('../db/db.js')
+const { getTodos, createTask, updateTask, deleteTask } = require('../db/db.js');
 
-
+//GET CRUD R = Read
 
 router.get('/todo', (req, res) => {
-    console.log('route')
-    getTodos()
-        .then(todos => {
-            console.log('hello then: ',todos)
-            res.json(todos)
-            //res.json means stringify the response & put it in JSON format so we can then deal with it this way
-        })
-        .catch(err => {
-            console.log('catch', err)
-            res.status(500).json({ message: 'Something broke' })
-        })
+	console.log('route');
+	getTodos()
+		.then((todos) => {
+			console.log('hello then: ', todos);
+			res.json(todos);
+			//res.json means stringify the response & put it in JSON format so we can then deal with it this way
+		})
+		.catch((err) => {
+			console.log('catch', err);
+			res.status(500).json({ message: 'Something broke' });
+		});
+});
+
+//POST CRUD C = Create
+
+router.post('/todo', (req, res) => {
+    const newTask = req.body;
+    //what the use types in
+	createTask(newTask)
+		.then((id) => {
+			res.json({ id: id });
+		})
+		.catch((err) => {
+			console.log('catch', err);
+			res.status(500).json({ message: 'Something broke' });
+		});
+});
+
+// PATCH CRUD U = UPDATE
+
+router.patch('/todo/:id', (req, res) => {
+    const id = req.params.id
+    const updatedTask = req.body
+    //  update by the id of the task
+    // with req.body that the user types in
+
+    updateTask(id, updatedTask)
+    .then( () => {
+        res.json({this:'works'})
+        // send back an empty body or an object (for no reason, just to tell us its updating the database)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ message: 'Something broke' })
+    })
+})
+
+// CRUD D = DELETE
+
+router.delete('/todo/:id', (req, res) => {
+    const id = req.params.id
+    deleteTask(id)
+    .then( () => {
+        res.json({this:'deletes'})
+        // send back an empty body or an object (for no reason, just to tell us its updating the database)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ message: 'Something broke' })
+    })
 })
 
 
-
-
-
-module.exports = router
+module.exports = router;
